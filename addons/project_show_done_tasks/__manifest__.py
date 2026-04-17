fe7b42295d0351c5bc2e6873e203414d8addaa68
victorimannuel <ops@imannuelvictor.com>
2026-04-17 12:44:32 +0700

    feat: change task kanban progressbar colors

---
+++ addons/project_show_done_tasks/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Project Show Done Tasks",
+    "version": "19.0.1.0.0",
+    "category": "Project",
+    "summary": "Show done tasks by default in project task view",
+    "author": "Custom",
+    "depends": ["project"],
+    "data": [
+        "views/project_task_action_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
