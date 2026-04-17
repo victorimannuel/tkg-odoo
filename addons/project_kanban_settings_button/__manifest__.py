ff111622f915d20c5456632a3c4b48fdf9ec7b4e
victorimannuel <ops@imannuelvictor.com>
2026-04-17 12:43:44 +0700

    feat: show kanban settings buttons in kanbar card

---
+++ addons/project_kanban_settings_button/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Project Kanban Settings Button",
+    "version": "19.0.1.0.0",
+    "category": "Project",
+    "summary": "Show a direct Settings button on project kanban cards",
+    "author": "Custom",
+    "depends": ["project"],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
